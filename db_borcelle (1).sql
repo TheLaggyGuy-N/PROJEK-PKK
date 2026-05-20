eac152c774fa55d4276bdae283c04c6bb97b3341
Nick <nickho0988@gmail.com>
2026-05-20 09:57:25 +0700

    Add files via upload

---
+++ db_borcelle (1).sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 20, 2026 at 02:56 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_borcelle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `id_akun` int NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gmail` varchar(50) NOT NULL,
+  `no_tlp` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`id_akun`, `username`, `password`, `gmail`, `no_tlp`, `nama`, `alamat`, `role`) VALUES
+(1, 'Nickholas', 'user123', 'Baks0En4k@gmail.com', '1133333333333', 'helo', 'depok', 'user'),
+(2, 'cihuh', 'user123', 'nickho0988@gmail.com', '', 'Rhajju Nickholas Putra', 'depok', 'user'),
+(3, 'Ganendra', 'admin123', 'ganendra123@gmail.com', '12345678', 'Ganendra', 'Depok', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id_keranjang` int NOT NULL,
+  `id_produk` int NOT NULL,
+  `jumlah` varchar(100) NOT NULL,
+  `id_akun` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga` varchar(50) NOT NULL,
+  `warna` varchar(100) NOT NULL,
+  `ukuran` varchar(100) NOT NULL,
+  `deskripsi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `stok` varchar(50) NOT NULL,
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga`, `warna`, `ukuran`, `deskripsi`, `stok`, `foto`) VALUES
+(3, 'Gelang', '7000', 'ireng', '12', 'jujur', '12', 'gelang1.jpeg'),
+(4, 'Gelang3', '9000', 'hijau', '12', 'cantikk', '20', 'gelang3.jpeg'),
+(5, 'Gelang3', '7000', 'pink', '17', 'Gelang cantik', '6', 'gelang2.jpeg'),
+(6, 'Gelang', '13000', 'hitam', '12', 'Gelang cantik', '4', 'gelang4.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int NOT NULL,
+  `id_akun` int NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `jumlah` int NOT NULL,
+  `total_bayar` int NOT NULL,
+  `alamat` text NOT NULL,
+  `tgl_pesan` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_akun`, `nama_produk`, `jumlah`, `total_bayar`, `alamat`, `tgl_pesan`) VALUES
+(1, 1, 'Gelang3', 1, 7000, 'depok', '2026-05-19 19:19:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id_akun`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id_akun` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
